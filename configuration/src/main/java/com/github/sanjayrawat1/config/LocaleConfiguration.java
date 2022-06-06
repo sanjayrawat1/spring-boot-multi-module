484ecb7010aa944698d1be47ccea4645f84d388a
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2022-06-06 07:16:31 +0530

    add date time and locale configuration

---
+++ configuration/src/main/java/com/github/sanjayrawat1/config/LocaleConfiguration.java
@@ -0,0 +1,28 @@
+package com.github.sanjayrawat1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Locale Configuration.
+ *
+ * @author Sanjay Singh Rawat
+ */
+public class LocaleConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+}
